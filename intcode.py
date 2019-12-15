2d5e13e4e310bab53826e8d36bf3b3f40048c14b
Jack <djchill10@gmail.com>
2019-12-15 18:24:53 +0000

    part 1 of day 5 + refactoring intcode. day 2 tests turned into intcode tests.

--- intcode.py
+++ intcode.py
@@ -1,19 +1,85 @@
+# Add:          opcode 1
+# Multiply:     opcode 2
+# Input:        opcode 3
+# Output:       opcode 4
+
+# opcode param lengths
+num_params = {
+    'ADD': 3,
+    'MULTIPLY': 3,
+    'INPUT': 1,
+    'OUTPUT': 1
+}
+
+# opcode param modes
+POSITION_MODE = 0
+IMMEDIATE_MODE = 1
+
+
+
+def execute_add(intcode, index):
+    numberA = intcode[intcode[index + 1]]
+    numberB = intcode[intcode[index + 2]]
+    intcode[intcode[index + 3]] = numberA + numberB
+    index += 4
+    return intcode, index
+
+
+def execute_multiply(intcode, index):
+    numberA = intcode[intcode[index + 1]]
+    numberB = intcode[intcode[index + 2]]
+    intcode[intcode[index + 3]] = numberA * numberB
+    index += 4
+    return intcode, index
+
+
+def execute_input(intcode, index):
+    print("Input:")
+    # user_input = input()
+    user_input = '1'
+    ## user input set to 1 for day 5 + tests for intcode!
+
+    intcode[intcode[index + 1]] = int(user_input.strip())
+    index += 2
+    return intcode, index
+
+
+def execute_output(intcode, index):
+    print("Output: ", intcode[intcode[index + 1]])
+    index += 2
+    return intcode, index
+
+
+def execute_done(intcode):
+    return intcode[0]
+
+
 def intcode(input_arr):
-    arr = input_arr[:]
+    intcode = input_arr[:]
+    index = 0
 
-    for index in range(0, len(arr), 4):
-        operator = arr[index]
+    while index <= len(intcode):
+        # print(index, len(intcode))
+        opcode = intcode[index]
+        # print ('opcode', opcode)
 
-        if operator == 99:
-            return arr[0]
+        if opcode == 99:
+            return execute_done(intcode)
 
-        numberA = arr[arr[index + 1]]
-        numberB = arr[arr[index + 2]]
+        if opcode == 1:
+            # print('add')
+            intcode, index = execute_add(intcode, index)
+            # print(intcode, index)
 
-        if operator == 1:
-            arr[arr[index + 3]] = numberA + numberB
+        if opcode == 2:
+            # print('multiply')
+            intcode, index = execute_multiply(intcode, index)
+            # print(intcode, index)
 
-        if operator == 2:
-            arr[arr[index + 3]] = numberA * numberB
+        if opcode == 3:
+            intcode, index = execute_input(intcode, index)
 
-    return arr[0]
+        if opcode == 4:
+            intcode, index = execute_output(intcode, index)
+
+    return execute_done(intcode)
---
+++ test_intcode.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+import intcode
+
+class Test(TestCase):
+    def test_day02(self):
+        self.assertEqual(2, intcode.intcode([1,0,0,0,99]))
+
+        self.assertEqual(2, intcode.intcode([2,3,0,3,99]))
+
+        self.assertEqual(2, intcode.intcode([2,4,4,5,99,0]))
+
+        self.assertEqual(30, intcode.intcode([1,1,1,4,99,5,6,0,99]))
+
+    def test_day05(self):
+        self.assertEqual(1, intcode.intcode([3,0,4,0,99]))

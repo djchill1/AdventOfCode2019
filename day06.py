345a3299afa4cf9860e87954a2c3844546897475
Jack <djchill10@gmail.com>
2019-12-17 23:31:54 +0000

    day 6 solution

--- day06.py
+++ day06.py
@@ -1,11 +1,23 @@
 with open('6_input.txt') as fp:
     data = fp.read().splitlines()
 
-#print(data)
+
+# print(data)
 
 def split_orbits(value):
     centre, orbiter = value.split(')')
     return centre, orbiter
+
+
+def all_objects(map):
+    centres = []
+    orbiters = []
+    for value in map:
+        centre, orbiter = split_orbits(value)
+        centres.append(centre)
+        orbiters.append(orbiter)
+    all = list(set(centres + orbiters))
+    return all
 
 
 def direct_orbits(map, centres):
@@ -19,27 +31,127 @@ def direct_orbits(map, centres):
             direct_orbits_count.append(1)
     return sum(direct_orbits_count), centres
 
-print('# direct orbits: ', direct_orbits(data, []))
 
 def indirect_orbits(map, centres):
     indirect_orbits_count = []
     centres_all = []
     orbiters_all = []
+    all = all_objects(map)
     for i in map:
         centre, orbiter = split_orbits(i)
         centres_all.append(centre)
         orbiters_all.append(orbiter)
-    for i in centres:
-        current_centre = i
+
+    for i in all:
+        current_object = i
         if i in orbiters_all:
-            index = centres_all[orbiters_all.index(i)]
-            print(index)
+            current_object = centres_all[orbiters_all.index(i)]
+            print('current object: ', current_object)
+            while current_object in orbiters_all:
+                current_object = centres_all[orbiters_all.index(current_object)]
+                indirect_orbits_count.append(current_object)
+    return len(indirect_orbits_count)
 
-        # centres here are now the orbiters
-        # find the index of the centre of any orbits of current_object
-        # add 1 to the indirect_orbits_count
-        # find any of the new "centre"s centres.
-        # continue until there are no more centres the "current_object" is orbiting.
-        # move onto next item in centres.
+    # centres here are now the orbiters
+    # find the index of the centre of any orbits of current_object
+    # add 1 to the indirect_orbits_count
+    # find any of the new "centre"s centres.
+    # continue until there are no more centres the "current_object" is orbiting.
+    # move onto next item in centres.
 
-indirect_orbits(data, [])
+
+def total_orbits(map):
+    direct, centres = direct_orbits(map, [])
+    indirect = indirect_orbits(map, centres)
+    return direct + indirect
+
+
+def you_to_santa(map):
+    dist_list = []
+    for i in map:
+        centre, orbiter = split_orbits(i)
+        dist_list.append((centre, orbiter, 1))
+    return dist_list
+
+
+from collections import defaultdict
+
+
+class Graph():
+    def __init__(self):
+        """
+        self.edges is a dict of all possible next nodes
+        e.g. {'X': ['A', 'B', 'C', 'E'], ...}
+        self.weights has all the weights between two nodes,
+        with the two nodes as a tuple as the key
+        e.g. {('X', 'A'): 7, ('X', 'B'): 2, ...}
+        """
+        self.edges = defaultdict(list)
+        self.weights = {}
+
+    def add_edge(self, from_node, to_node, weight):
+        # Note: assumes edges are bi-directional
+        self.edges[from_node].append(to_node)
+        self.edges[to_node].append(from_node)
+        self.weights[(from_node, to_node)] = weight
+        self.weights[(to_node, from_node)] = weight
+
+
+graph = Graph()
+
+
+def edge_to_graph(data):
+    for edge in you_to_santa(data):
+        graph.add_edge(*edge)
+
+
+# dijsktra code by Ben Keen.
+# http://benalexkeen.com/implementing-djikstras-shortest-path-algorithm-with-python/
+
+def dijsktra(graph, initial, end):
+    # shortest paths is a dict of nodes
+    # whose value is a tuple of (previous node, weight)
+    shortest_paths = {initial: (None, 0)}
+    current_node = initial
+    visited = set()
+
+    while current_node != end:
+        visited.add(current_node)
+        destinations = graph.edges[current_node]
+        weight_to_current_node = shortest_paths[current_node][1]
+
+        for next_node in destinations:
+            weight = graph.weights[(current_node, next_node)] + weight_to_current_node
+            if next_node not in shortest_paths:
+                shortest_paths[next_node] = (current_node, weight)
+            else:
+                current_shortest_weight = shortest_paths[next_node][1]
+                if current_shortest_weight > weight:
+                    shortest_paths[next_node] = (current_node, weight)
+
+        next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
+        if not next_destinations:
+            return "Route Not Possible"
+        # next node is the destination with the lowest weight
+        current_node = min(next_destinations, key=lambda k: next_destinations[k][1])
+
+    # Work back through destinations in shortest path
+    path = []
+    while current_node is not None:
+        path.append(current_node)
+        next_node = shortest_paths[current_node][0]
+        current_node = next_node
+    # Reverse path
+    path = path[::-1]
+    return path
+
+
+print('part a solution: ', total_orbits(data))
+
+
+def part_2_solution(data):
+    edge_to_graph(data)
+    total = dijsktra(graph, 'YOU', 'SAN')
+    print('path: ', total)
+    print('len: ', len(total) - 3)
+    return len(total) - 3
--- test_day06.py
+++ test_day06.py
@@ -1,9 +1,31 @@
 from unittest import TestCase
 import day06
 
+
 class Test(TestCase):
     def test_direct_orbits(self):
-        data = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L']
+        test_data = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L']
         centres = []
-        direct_orbits_count, centres = day06.direct_orbits(data, centres)
+        direct_orbits_count, centres = day06.direct_orbits(test_data, centres)
         self.assertEqual(11, direct_orbits_count)
+
+    def test_total_orbits(self):
+        test_data = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L']
+        total = day06.total_orbits(test_data)
+        self.assertEqual(42, total)
+
+
+class Test(TestCase):
+    def test_you_to_santa(self):
+        test_data = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L', 'K)YOU', 'I)SAN']
+        total = day06.you_to_santa(test_data)
+        self.assertEqual(
+            [('COM', 'B', 1), ('B', 'C', 1), ('C', 'D', 1), ('D', 'E', 1), ('E', 'F', 1), ('B', 'G', 1), ('G', 'H', 1),
+             ('D', 'I', 1), ('E', 'J', 1), ('J', 'K', 1), ('K', 'L', 1), ('K', 'YOU', 1), ('I', 'SAN', 1)], total)
+
+
+class Test(TestCase):
+    def test_part_2_solution(self):
+        test_data = ['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L', 'K)YOU', 'I)SAN']
+        total = day06.part_2_solution(test_data)
+        self.assertEqual(4, total)

bd9b265cc57f69961888fa34180c77a71d31411e
Jack Clare <jack.clare1@tesco.com>
2019-12-19 15:42:20 +0000

    getting closer to day7 part 2

--- day07.py
+++ day07.py
@@ -3,7 +3,7 @@ import itertools
 
 data = [3,8,1001,8,10,8,105,1,0,0,21,38,63,80,105,118,199,280,361,442,99999,3,9,102,5,9,9,1001,9,3,9,1002,9,2,9,4,9,99,3,9,1001,9,4,9,102,4,9,9,101,4,9,9,102,2,9,9,101,2,9,9,4,9,99,3,9,1001,9,5,9,102,4,9,9,1001,9,4,9,4,9,99,3,9,101,3,9,9,1002,9,5,9,101,3,9,9,102,5,9,9,101,3,9,9,4,9,99,3,9,1002,9,2,9,1001,9,4,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,99]
 
-def amplifier_controller(phase_setting, input_signal):
+def amplifier_controller(phase_setting, input_signal, repeating=False):
     # output from each version of intcode with phase setting is the next ones input
     phase = []
     thruster = []
@@ -11,15 +11,22 @@ def amplifier_controller(phase_setting, input_signal):
         # initialise input with first item
         current_input = 0
         # print('current_input: ', current_input)
-        for j in range(0, len(phase_setting[0])):
-            # print('phase setting = ', phase_setting[0])
-            # print('i[j] = ', i[j])
-            current_input = intcode.intcode(input_signal, 7, [i[j], current_input])
-            # print('current_input: ', current_input)
-            # print('\n/////////////\n')
+        if repeating == True:
+            for j in range(0, len(phase_setting[0])):
+                current_input = intcode.intcode(input_signal, 7, [i[j], current_input])
 
-        phase.append(i)
-        thruster.append(current_input)
+                phase.append(i)
+                thruster.append(current_input)
+        else:
+            for j in range(0, len(phase_setting[0])):
+                # print('phase setting = ', phase_setting[0])
+                # print('i[j] = ', i[j])
+                current_input = intcode.intcode(input_signal, 7, [i[j], current_input])
+                # print('current_input: ', current_input)
+                # print('\n/////////////\n')
+
+            phase.append(i)
+            thruster.append(current_input)
 
     return max(thruster)
 
@@ -32,3 +39,9 @@ phases = get_phase_settings([0,1,2,3,4], 5)
 
 parta = amplifier_controller(phases, data)
 print('part a: ', parta)
+
+
+b_phases = get_phase_settings([5,6,7,8,9], 5)
+partb = amplifier_controller(b_phases, data, True)
+print('part b: ', partb)
+
